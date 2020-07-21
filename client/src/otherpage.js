e249ae5896870a846506c736ae2639e6113e290e
serenitis <alen@panteongroup.com>
2020-07-21 15:44:08 +0200

    repair client2

---
+++ client/src/otherpage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default() =>{
+
+return (
+<div>
+    Some other page
+    <Link to="/">BACK</Link>
+</div>
+);
+
+
+}
+
+
